ad28caa476165f457d56d60bc04e5e8315ae7724
HP 840 G1 <hp840g1@HP-840-g1s-iMac.local>
2018-07-16 18:07:19 +0300

    view home, login, register

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- footer -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <main role="main" class="container">
+
+        <div class="starter-template">
+            <h1>Welcome to our company</h1>
+            <h2>Managment system</h2>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam officia excepturi non suscipit earum? Aperiam repellat necessitatibus incidunt corporis? Unde ex, nemo voluptatum ad, minus quasi modi laboriosam porro illum saepe esse a error! Officiis magnam rerum minus amet minima similique labore, eos molestiae. Expedita rerum optio quibusdam voluptas. Omnis nesciunt id sunt nisi provident quidem. Dolorum ipsa illum, quia, cupiditate consequuntur nulla, nobis ullam quidem, natus ab cumque iste eveniet suscipit officia porro. Ducimus aspernatur nam quibusdam obcaecati nisi, illo optio voluptatem eius dolore impedit sunt repellat nesciunt architecto unde quos non ullam, asperiores magni officia neque, atque ipsam.</p>
+        </div>
+
+    </main>
+
+@endsection
